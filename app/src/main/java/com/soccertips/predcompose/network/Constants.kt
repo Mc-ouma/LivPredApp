ea168d338be0178d6ac720a8f996879dd9890f8f
Crafton <159699093+Mc-ouma@users.noreply.github.com>
2025-01-20 10:52:08 +0300

    Update Constants.kt

--- app/src/main/java/com/soccertips/predcompose/network/Constants.kt
+++ app/src/main/java/com/soccertips/predcompose/network/Constants.kt
@@ -1,9 +1,9 @@
 package com.soccertips.predcompose.network
 
 object Constants {
-    const val BASE_URL: String = "https://v3.football.api-sports.io/"
-    const val API_KEY: String = "0880f79a66438c6e30f16c8ff7f6fe2d"
-    const val API_HOST: String = "v3.football.api-sports.io"
+    const val BASE_URL: String = System.getenv("BASE_URL") ?: "https://v3.football.api-sports.io/"
+    const val API_KEY: String = System.getenv("API_KEY") ?: ""
+    const val API_HOST: String = System.getenv("API_HOST") ?: "v3.football.api-sports.io"
     const val CACHE_MAX_AGE_SHORT = 60 * 60 // 1 hour in seconds
     const val CACHE_MAX_AGE_LONG = 2 * 60 * 60 // 2 hours in seconds
     const val CACHE_MAX_AGE_VERY_LONG = 24 * 60 * 60 // 24 hours in seconds
